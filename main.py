b083b04ffdcf5271c2c7b287650ca7de5c78dece
lex <a.zhuravlev@cdnvideo.com>
2021-10-19 01:59:02 +0300

    In this alternative branh stacktest i will create some unit test for my stack in this current non unit test i use some basic stack functionality all tested passed succsesfly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #realization of stack on python
 
-class nodeManager(data):
+class nodeManager():
 	def __init__ (self, *values):
 		self.data_all = []
 		for val in values:
@@ -14,7 +14,11 @@ class nodeManager(data):
 		self.head = len(self.data_all)
 		self.data_all.append(data)
 
+	def __str__(self):
+		return self.data_all.__str__()
 
-cal = node(123)
-cal.addElem()
+
+cal = nodeManager(123)
+cal.addElem(123)
+print(cal)
 
